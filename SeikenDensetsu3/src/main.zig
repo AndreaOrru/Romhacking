479e08c6ab5764e68ebacc14055d5d7710333f75
Andrea Orru <andrea@orru.io>
2019-01-03 17:58:20 +0000

    Fetch arguments

--- SeikenDensetsu3/src/main.zig
+++ SeikenDensetsu3/src/main.zig
@@ -1,5 +1,20 @@
-const warn = @import("std").debug.warn;
+const std = @import("std");
+const os = std.os;
+const assert = std.debug.assert;
+const warn = std.debug.warn;
 
-pub fn main() void {
-    warn("Hello, world!\n");
+pub fn main() !void {
+    // Initialize allocator.
+    var direct_allocator = std.heap.DirectAllocator.init();
+    defer direct_allocator.deinit();
+    var arena = std.heap.ArenaAllocator.init(&direct_allocator.allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+
+    // Fetch arguments.
+    const args = try os.argsAlloc(allocator);
+    defer os.argsFree(allocator, args);
+    assert(args.len == 2);
+
+    warn("{}", args[1]);
 }
