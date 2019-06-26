50fa096de904d95fee00d99573df8031ab00b435
Andrea Orru <andrea@orru.io>
2019-06-26 13:43:04 +0100

    Zero

--- BahamutLagoon/extractor.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-
-std::string extract(std::vector<uint8_t>* data);
--- BahamutLagoon/reinserter.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-
-void reinsert(std::vector<uint8_t>* data, std::string text);
