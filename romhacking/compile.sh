2022c7c16e669ce61bd06f8e751470fb0717213c
Andrea Orru <andrea@orru.io>
2020-04-27 03:09:51 +0900

    Compile.sh

--- romhacking/compile.sh
+++ romhacking/compile.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -x "$(command -v clang++)" ]; then
     CXX=clang++
@@ -6,11 +6,15 @@ else
     CXX=g++
 fi
 
-$CXX                             \
-    -std=c++17 -Wall             \
-    -Ofast -march=native -fPIC   \
-    -lboost_python38 -lpython3.8 \
-    -I/usr/include/python3.8     \
-    -shared                      \
-    -o bytepair.so               \
+PYTHON_INCLUDE="$(python3-config --includes | cut -d ' ' -f 1)"
+PYTHON_LIB="${PYTHON_INCLUDE##*/}"
+BOOST_LIB="boost_${PYTHON_LIB//.}"
+
+$CXX                            \
+    -std=c++17 -Wall            \
+    -Ofast -march=native -fPIC  \
+    -l$BOOST_LIB -l$PYTHON_LIB  \
+    $PYTHON_INCLUDE             \
+    -shared                     \
+    -o bytepair.so              \
     bytepair.cpp
