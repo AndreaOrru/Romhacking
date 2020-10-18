9d79c661a08b6cd4764b117f28d3f6fcb45e0771
Andrea Orru <andrea@orru.io>
2020-10-18 22:46:31 +0900

    Add table

--- supermariorpg/generate_bins.py
+++ supermariorpg/generate_bins.py
@@ -2,12 +2,16 @@
 
 text = [x for x in open("dict.txt").read().split("\n") if x]
 
+table = [tuple(x.split("=", 1)) for x in open("smrpg.tbl").read().split("\n") if x]
+table = {v: int(k, 16) for k, v in table}
+
 bin_dict = bytearray()
 bin_ptrs = bytearray()
 
 ptr = 0
 for word in text:
-    bin_dict.extend(word.encode("ascii"))
+    bin_word = bytearray([table[x] for x in word])
+    bin_dict.extend(bin_word)
     bin_dict.append(0x00)
 
     bin_ptrs.append(ptr & 0xFF)
