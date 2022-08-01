a157316f75bf51608500c99830a29446eea35e5d
Andrea Orru <andrea@orru.io>
2022-08-02 00:16:29 +0900

    Add backup compression routine in Python

--- romhacking/compression.py
+++ romhacking/compression.py
@@ -1,6 +1,66 @@
+from copy import deepcopy
+from collections import defaultdict
 from typing import List, Tuple
 
-from romhacking.bytepair import bytepairCompress  # noqa
+from romhacking.utils import pairwise
+
+N_SYMBOLS = 0x1000
+
+
+def replacePair(data: List[int], pair: int, symbol: int):
+    i = 0
+    while True:
+        if i >= len(data) - 1:
+            return
+        if (data[i] == pair & 0xFFFF) and (data[i + 1] == pair >> 16):
+            data[i] = symbol
+            data.pop(i + 1)
+        i += 1
+
+
+def replacePairBlocks(blocks_data: List[List[List[int]]], pair: int, symbol: int):
+    for block_data in blocks_data:
+        for sentence_data in block_data:
+            replacePair(sentence_data, pair, symbol)
+
+
+try:
+    from romhacking.bytepair import bytepairCompress  # noqa
+except ModuleNotFoundError:
+
+    def bytepairCompress(
+        blocks_data: List[List[List[int]]],
+    ) -> Tuple[List[Tuple[int, int]], List[List[List[int]]]]:
+
+        blocks_data = deepcopy(blocks_data)
+        replacePairBlocks(blocks_data, 0x00FF00FF, 0x100)
+
+        symbol = 0x101
+        dictionary = []
+
+        while symbol < N_SYMBOLS:
+            occ = defaultdict(int)
+            max_pair = 0
+            max_occ = 0
+
+            for block_data in blocks_data:
+                for sentence in block_data:
+                    for a, b in pairwise(sentence):
+                        curr_pair = (b << 16) | a
+                        occ[curr_pair] += 1
+                        curr_occ = occ[curr_pair]
+                        if curr_occ > max_occ:
+                            max_occ = curr_occ
+                            max_pair = curr_pair
+
+            if max_occ < 4:
+                break
+
+            replacePairBlocks(blocks_data, max_pair, symbol)
+            dictionary.append((max_pair & 0xFFFF, max_pair >> 16))
+            symbol += 1
+
+        return dictionary, blocks_data
 
 
 def encode_12bit(strings: List[List[int]]) -> Tuple[List[int], List[int]]:
--- romhacking/tests/test_compression.py
+++ romhacking/tests/test_compression.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
 
-from romhacking.compression import bytepairCompress, encode_12bit
+from romhacking.compression import (
+    bytepairCompress,
+    encode_12bit,
+    replacePair,
+    replacePairBlocks,
+)
 
 
 class CompressionTestCase(TestCase):
@@ -15,6 +20,39 @@ class CompressionTestCase(TestCase):
         self.assertEqual(indexes[1], base_index + (2 * 12))
 
         self.assertListEqual(data, [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC])
+
+    def test_replacePair(self):
+        data = [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC]
+        replacePair(data, 0x0034_0012, 0x100)
+        replacePair(data, 0x0078_0056, 0x101)
+        replacePair(data, 0x00BC_009A, 0x102)
+        replacePair(data, 0x0101_0100, 0x103)
+        replacePair(data, 0x0102_0103, 0x104)
+        self.assertListEqual(data, [0x104])
+
+    def test_replacePairBlocks(self):
+        data = [
+            [
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC],
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC],
+            ],
+            [
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC],
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC],
+            ],
+        ]
+        replacePairBlocks(data, 0x0034_0012, 0x100)
+        replacePairBlocks(data, 0x0078_0056, 0x101)
+        replacePairBlocks(data, 0x00BC_009A, 0x102)
+        replacePairBlocks(data, 0x0101_0100, 0x103)
+        replacePairBlocks(data, 0x0102_0103, 0x104)
+        self.assertListEqual(
+            data,
+            [
+                [[0x104], [0x104]],
+                [[0x104], [0x104]],
+            ],
+        )
 
     def test_bytepairCompress(self):
         string1 = "low low low low entropy entropy entropy entropy"
